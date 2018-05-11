58e921cd81dfa10260a3c127d764e85485ec7f38
Matas Savickis <savickismatas@gmail.com>
2018-05-11 04:09:24 +0300

    Prep work

---
+++ Slidinejimoi kurorto aplikacija/SkiingApp/SkiingApp/app/App.jsx
@@ -0,0 +1,14 @@
+import { hot } from 'react-hot-loader';
+import React from 'react';
+import './styles.css';
+import { Switch } from 'react-router';
+
+const App = () => (
+    <div>
+        <Switch>
+
+        </Switch>
+    </div>
+);
+
+export default hot(module)(App);
