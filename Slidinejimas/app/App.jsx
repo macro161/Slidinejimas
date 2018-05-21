4ebd31a70a09653326258e573df1d636ede966a2
Matas Savickis <savickismatas@gmail.com>
2018-05-21 21:31:44 +0300

    asdas

--- Slidinejimas/app/App.jsx
+++ Slidinejimas/app/App.jsx
@@ -2,18 +2,13 @@
 import React from 'react';
 import './styles.css';
 import Login from './components/login/Login';
-import Delivery from './components/delivery/Delivery';
 import MasterAdmin from './components/AdminPage/masteradmin/MasterAdmin';
-import CheckOut from './components/checkOut/CheckOut';
 import { Switch } from 'react-router';
-import CheckIn from './components/checkIn/CheckIn';
 import Sites from './components/AdminPage/Sites/Sites';
 import { Provider } from 'react-redux';
 import store, { history } from './components/store';
-import Report from './components/AdminPage/report/Report';
 import { ConnectedRouter } from 'react-router-redux';
 import ErrorPage from './components/Error/Error404';
-import Camera from './components/camera/Camera';
 import PublicLayoutRoute from './components/layout/PublicLayout';
 import AdminLayoutRoute from './components/layout/AdminLayout';
 
@@ -23,14 +18,9 @@ const App = () => (
       <div>
         <Switch>
           <PublicLayoutRoute exact path="/" component={Login} />
-          <PublicLayoutRoute path="/delivery" component={Delivery} />
-          <PublicLayoutRoute path="/checkin" component={CheckIn} />
-          <PublicLayoutRoute path="/checkout" component={CheckOut} />
           <PublicLayoutRoute path="/login" component={Login} />
-          <PublicLayoutRoute path="/TakePhoto" component={Camera} />
           <AdminLayoutRoute path="/masteradmin" component={MasterAdmin} />
           <AdminLayoutRoute path="/sites" component={Sites} />
-          <AdminLayoutRoute path="/report" component={Report} />
           <PublicLayoutRoute path="*" component={ErrorPage} />
         </Switch>
       </div>
